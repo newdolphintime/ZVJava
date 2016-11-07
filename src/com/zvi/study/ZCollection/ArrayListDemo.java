876775e6072c439d7db61f781f92e4172f034f18
zhangwei123 <1551583950@qq.com>
2016-11-07 23:16:57 +0800

    集合arraylist、linklist

--- src/com/zvi/study/ZCollection/ArrayListDemo.java
+++ src/com/zvi/study/ZCollection/ArrayListDemo.java
@@ -2,25 +2,57 @@ package com.zvi.study.ZCollection;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class ArrayListDemo {
+	/*
+	 * LIST 特点 元素有序，可以重复  因为该集合有索引
+	 *     ARRAYLIST 底层是数组 查询速度快 增删速度慢 线程不同步 效率高 默认长度是10 超过10 创建数组加50% 再把原来的 拷贝进去
+	 *     LINKLIST 底层是链表 查询速度满 增删速度快
+	 *     VECTOR 重复 老东西 线程同步
+	 * */
 	public static void main(String[] args)
 
 	{
-		methodBase();
-
+		//arrarymethodBase();
+		arraymethodPro();
+		linkmethodPro();
 	}
 
-	private static void methodBase() {
+	private static void arraymethodPro() {
+		// TODO Auto-generated method stub
+		ArrayList al =new ArrayList();
+		al.add("java 1");
+		al.add("java 2");
+		al.add("java 3");
+		al.add("java 4");
+		
+		ListIterator li =al.listIterator() ;
+		
+		while (li.hasNext()){
+			Object obj=li.next();
+			System.out.println(obj);
+			if(obj.equals("java 3")){
+				li.set("java 3-0");
+			}
+		}
+		System.out.println(al);
+	}
+
+	private static void arraymethodBase() {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
 		ArrayList al = new ArrayList();
-
+		//添加元素
 		al.add("world one ");
 
 		al.add("world two");
 		al.add("world three ");
 		al.add("world four ");
+		//按照位置添加
+		al.add(1, "java2");
+		System.out.println(al.get(1));
 		System.out.println("world three是否存在" + al.contains("world three "));
 		System.out.println("集合是否为空" + al.isEmpty());
 		System.out.println(al);
@@ -47,5 +79,13 @@ public class ArrayListDemo {
 			System.out.println(it.next());
 		}
 	}
+	 private  static void linkmethodPro(){
+		 LinkedList li =new LinkedList();
+		 li.addFirst("j1");
+		 li.addFirst("j2");
+		 li.addFirst("j3");
+		 li.addFirst("j4");
+		 System.out.println(li);
+	 }
 
 }
