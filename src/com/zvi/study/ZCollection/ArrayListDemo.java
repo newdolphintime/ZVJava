8eb1987c829a29e59972d305a130e71dbb11024b
zhangwei123 <1551583950@qq.com>
2016-11-08 23:16:13 +0800

    hashset以及 arrylist的contains比较逻辑以及hashset的去重逻辑

--- src/com/zvi/study/ZCollection/ArrayListDemo.java
+++ src/com/zvi/study/ZCollection/ArrayListDemo.java
@@ -7,33 +7,56 @@ import java.util.ListIterator;
 
 public class ArrayListDemo {
 	/*
-	 * LIST 特点 元素有序，可以重复  因为该集合有索引
-	 *     ARRAYLIST 底层是数组 查询速度快 增删速度慢 线程不同步 效率高 默认长度是10 超过10 创建数组加50% 再把原来的 拷贝进去
-	 *     LINKLIST 底层是链表 查询速度满 增删速度快
-	 *     VECTOR 重复 老东西 线程同步
-	 * */
+	 * LIST 特点 元素有序，可以重复 因为该集合有索引 ARRAYLIST 底层是数组 查询速度快 增删速度慢 线程不同步 效率高 默认长度是10
+	 * 超过10 创建数组加50% 再把原来的 拷贝进去 LINKLIST 底层是链表 查询速度满 增删速度快 VECTOR 重复 老东西 线程同步
+	 */
 	public static void main(String[] args)
 
 	{
-		//arrarymethodBase();
-		arraymethodPro();
-		linkmethodPro();
+		// arrarymethodBase();
+		// arraymethodPro();
+		// linkmethodPro();
+		arraylistProPro();
+	}
+
+	private static void arraylistProPro() {
+		// TODO Auto-generated method stub
+		ArrayList al = new ArrayList();
+		ArrayList newal = new ArrayList();
+		al.add(new Person("j1", 1));
+		al.add(new Person("j1", 1));
+		al.add(new Person("j3", 3));
+		al.add(new Person("j4", 4));
+		Iterator it = al.iterator();
+		while (it.hasNext()) {
+			Person p = (Person) it.next();
+			if (!newal.contains(p))//會調用Person的equal方法
+				newal.add(p);
+			// System.out.println(p.name+"+++++"+p.age);
+		}
+		Iterator newit = newal.iterator();
+		while (newit.hasNext()) {
+			Person p = (Person) newit.next();
+
+			System.out.println("去重后" + p.name + "+++++" + p.age);
+		}
+
 	}
 
 	private static void arraymethodPro() {
 		// TODO Auto-generated method stub
-		ArrayList al =new ArrayList();
+		ArrayList al = new ArrayList();
 		al.add("java 1");
 		al.add("java 2");
 		al.add("java 3");
 		al.add("java 4");
-		
-		ListIterator li =al.listIterator() ;
-		
-		while (li.hasNext()){
-			Object obj=li.next();
+
+		ListIterator li = al.listIterator();
+
+		while (li.hasNext()) {
+			Object obj = li.next();
 			System.out.println(obj);
-			if(obj.equals("java 3")){
+			if (obj.equals("java 3")) {
 				li.set("java 3-0");
 			}
 		}
@@ -44,13 +67,13 @@ public class ArrayListDemo {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
 		ArrayList al = new ArrayList();
-		//添加元素
+		// 添加元素
 		al.add("world one ");
 
 		al.add("world two");
 		al.add("world three ");
 		al.add("world four ");
-		//按照位置添加
+		// 按照位置添加
 		al.add(1, "java2");
 		System.out.println(al.get(1));
 		System.out.println("world three是否存在" + al.contains("world three "));
@@ -73,19 +96,20 @@ public class ArrayListDemo {
 		al.retainAll(al1);
 		System.out.println(al);// al保留交集
 		System.out.println(al1);
-		//接口只能指向子类对象
+		// 接口只能指向子类对象
 		Iterator it = al1.iterator();
 		while (it.hasNext()) {
 			System.out.println(it.next());
 		}
 	}
-	 private  static void linkmethodPro(){
-		 LinkedList li =new LinkedList();
-		 li.addFirst("j1");
-		 li.addFirst("j2");
-		 li.addFirst("j3");
-		 li.addFirst("j4");
-		 System.out.println(li);
-	 }
+
+	private static void linkmethodPro() {
+		LinkedList li = new LinkedList();
+		li.addFirst("j1");
+		li.addFirst("j2");
+		li.addFirst("j3");
+		li.addFirst("j4");
+		System.out.println(li);
+	}
 
 }
