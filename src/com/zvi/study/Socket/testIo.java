496de4abae03bce2c54ccee41dbf4ef1813bf19a
zhangwei123 <1551583950@qq.com>
2017-04-02 21:06:32 +0800

    SYSTEM.IN

---
+++ src/com/zvi/study/Socket/testIo.java
@@ -0,0 +1,22 @@
+package com.zvi.study.Socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Zv on 2017/04/02.
+ */
+public class testIo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bfre = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("-----这一步----");
+
+        String line = null;
+        while ((line = bfre.readLine())!=null){
+            System.out.println(line);
+        }
+
+    }
+
+}
