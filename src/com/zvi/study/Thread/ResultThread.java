423ae4ad229ecf0fb203c0e0dd1d6ad05d3a6d54
zhangwei123 <1551583950@qq.com>
2017-04-16 12:45:27 +0800

    有返回值的线程

---
+++ src/com/zvi/study/Thread/ResultThread.java
@@ -0,0 +1,56 @@
+package com.zvi.study.Thread;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * Created by Zv on 2017/04/16.
+ * 使用ExecutorService、Callable、Future实现有返回结果的多线程
+ */
+public class ResultThread {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        System.out.println("----程序开始运行----");
+        Date date1 = new Date();
+        int taskSize = 5;
+        // 创建一个线程池
+        ExecutorService pool = Executors.newFixedThreadPool(taskSize);
+        // 创建多个有返回值的任务
+        List<Future> list = new ArrayList<Future>();
+        for (int i = 0; i < taskSize; i++) {
+            Callable c = new MyCallable(i + " ");
+            // 执行任务并获取Future对象
+            Future f = pool.submit(c);
+            //System.out.println(">>>" + f.get().toString());
+            list.add(f);
+        }
+        // 关闭线程池
+        pool.shutdown();
+        for (Future f : list) {
+            // 从Future对象上获取任务的返回值，并输出到控制台
+            System.out.println(">>>" + f.get().toString());
+        }
+        Date date2 = new Date();
+        System.out.println("----程序结束运行----，程序运行时间【" + (date2.getTime() - date1.getTime()) + "毫秒】");
+    }
+}
+
+class MyCallable implements Callable<Object> {
+    private String taskNum;
+
+    MyCallable(String taskNum) {
+        this.taskNum = taskNum;
+    }
+
+    @Override
+    public Object call() throws Exception {
+        System.out.println(">>>" + taskNum + "任务启动");
+        Date dateTmp1 = new Date();
+        Thread.sleep(1000);
+        Date dateTmp2 = new Date();
+        long time = dateTmp2.getTime() - dateTmp1.getTime();
+        System.out.println(">>>" + taskNum + "任务终止");
+        return taskNum + "任务返回运行结果,当前任务时间【" + time + "毫秒】";
+    }
+}
